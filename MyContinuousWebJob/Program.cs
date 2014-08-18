51af0c527333f363c81f225b02b7d34b7babe152
davidebbo <david.ebbo@microsoft.com>
2014-08-18 16:51:23 -0700

    Add continuous WebJob

---
+++ MyContinuousWebJob/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyContinuousWebJob
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+            for (; ; )
+            {
+                Console.WriteLine("It's {0}", DateTime.Now);
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
