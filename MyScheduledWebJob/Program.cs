d64a85418cb01585e2048ad06a8b697118b3c129
davidebbo <david.ebbo@microsoft.com>
2014-08-18 17:42:30 -0700

    Add scheduled job

---
+++ MyScheduledWebJob/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyScheduledWebJob
+{
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Scheduled job!");
+        }
+    }
+}
